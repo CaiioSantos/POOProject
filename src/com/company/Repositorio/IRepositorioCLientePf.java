099218a40938635a1f5ee7f2a8f331d54ecdb264
CaiioSantos <caiohgs13@gmail.com>
2018-11-22 18:13:33 -0300

    codando a camda de Repositorio

--- src/com/company/Repositorio/IRepositorioCLientePf.java
+++ src/com/company/Repositorio/IRepositorioCLientePf.java
@@ -1,4 +1,18 @@
 package com.company.Repositorio;
 
+import com.company.Execao.ClientePfExeption;
+import com.company.Execao.RepositorioClientePfExeption;
+import com.company.model.ClientePf;
+
+
+import java.util.ArrayList;
+
 public interface IRepositorioCLientePf {
+    void inserirClientePf   (ClientePf obj) throws ClientePfExeption, RepositorioClientePfExeption;
+    void deletarClientePf   (ClientePf obj) throws ClientePfExeption, RepositorioClientePfExeption;
+    void atualizarClientePf (ClientePf obj) throws ClientePfExeption, RepositorioClientePfExeption;
+    ClientePf pesquisarClientePorNome (String obj) throws  ClientePfExeption, RepositorioClientePfExeption;
+    ClientePf pesquisarClientePorCpf  (String obj) throws  ClientePfExeption, RepositorioClientePfExeption;
+    ArrayList<ClientePf> listarClientes() throws ClientePfExeption, RepositorioClientePfExeption;
+
 }
--- src/com/company/Repositorio/RepositorioClientePf.java
+++ src/com/company/Repositorio/RepositorioClientePf.java
@@ -1,12 +1,42 @@
 package com.company.Repositorio;
 
+import com.company.Execao.ClientePfExeption;
+import com.company.Execao.RepositorioClientePfExeption;
 import com.company.model.ClientePf;
 
 import java.util.ArrayList;
 
 public class RepositorioClientePf {
-    private ArrayList<ClientePf> clientePfArrayList;
-    private static RepositorioClientePf instacia;
+    private ArrayList clientePfArrayList;
+    private static RepositorioClientePf instancia;
 
+    public static RepositorioClientePf getInstance() throws ClientePfExeption, RepositorioClientePfExeption {
+        if (instancia ==null){
+            instancia = new RepositorioClientePf();
+        }
 
+        return instancia;
+    }
+
+    public RepositorioClientePf() throws ClientePfExeption, RepositorioClientePfExeption{
+        if(this.clientePfArrayList == null){
+            this.clientePfArrayList = new ArrayList();
+        }
+    }
+
+    public void inserirClientePf(ClientePf clientePf) throws ClientePfExeption,RepositorioClientePfExeption{
+        this.clientePfArrayList.add(clientePf);
+    }
+    public void deletarClientePf(ClientePf clientePf) throws ClientePfExeption, RepositorioClientePfExeption{
+        for (int i=0; i< this.clientePfArrayList.size(); i++){
+            if(this.clientePfArrayList.get(i) == clientePf ){
+                this.clientePfArrayList.set(i, clientePf);
+                break;
+            }
+        }
+
+        if (clientePf == null){
+            throw new RepositorioClientePfExeption("Cliente Não foi removido, Ele nao foi encontrado na nossa Lista de clientes.");
+        }
+    }
 }
